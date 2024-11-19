89bc6d4a56305e65cb8d0362b561dc4ab3c185ae
Semin Bae <semin.bae.1999@gmail.com>
2024-11-19 13:15:47 +0900

    socket connect start

--- C&C++/0. hello/hello.c
+++ C&C++/0. hello/hello.c
@@ -1,6 +1,31 @@
-#include <stdio.h> // 라이브러리 추가 (print f 등등)
 
-int main(void){
-    printf("hello world");
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+
+int sigStrlen(char *str) {
+    int i;
+    for(i = 0; str[i]; i++) ;
+    return i;
+}
+
+void sigPuts(char *str) { write(1, str, sigStrlen(str)); }
+
+void handler(int sig) { sigPuts("Caught SIGINT!\n"); }
+
+
+int main() {
+    signal(SIGINT, handler); //register handler
+    sigset_t mask, oldmask; // prepare mask set
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    //Try Ctrl-C with and without this line
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+    printf("Before sleep\n");
+    sleep(3);
+    printf("After sleep\n");
+    sigprocmask(SIG_SETMASK, &oldmask, NULL);
     return 0;
 }
